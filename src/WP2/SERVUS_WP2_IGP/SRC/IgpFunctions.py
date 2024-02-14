de30cc28620159fc78389e5d148c823b0a396cf3
AstroBuey <tean88@gmail.com>
2024-02-13 18:12:10 -0600

    Add initial WP2 files

---
+++ src/WP2/SERVUS_WP2_IGP/SRC/IgpFunctions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+########################################################################
+# SBPT/SRC/IgpFunctions.py:
+# This script defines all internal functions of IgpPerformance Module
+#
+#  Project:        SBPT
+#  File:           IgpFunctions.py
+#  Date(YY/MM/DD): 20/07/11
+#
+#   Author: GNSS Academy
+#   Copyright 2021 GNSS Academy
+########################################################################
+
+
+# Import External and Internal functions and Libraries
+#----------------------------------------------------------------------
+
+
+
+
+########################################################################
+#END OF IGP FUNCTIONS MODULE
+########################################################################
---
+++ src/WP2/SERVUS_WP2_IGP/SRC/IgpPerformances.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+########################################################################
+# SBPT/SRC/IgpPerformances.py:
+# This function is the Main Function of IGP PERF Module
+#
+#  Project:        SBPT/SERVUS
+#  File:           IgpPerforamnces.py
+#  Date(YY/MM/DD): 20/07/11
+#
+#   Author: GNSS Academy
+#   Copyright 2021 GNSS Academy
+#
+# -----------------------------------------------------------------
+# Date       | Author             | Action
+# -----------------------------------------------------------------
+#
+# Usage:
+# i.e: IgpPerformances $SCEN_PATH
+########################################################################
+
+# Import External and Internal functions and Libraries
+#----------------------------------------------------------------------
+
+
+
+#####################################################################
+#END OF IGP PERFORMANCES MODULE
+#######################################################
